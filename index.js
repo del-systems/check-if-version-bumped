58a1a122210eddbea5c07a2e54078ff0dae1e4f3
Stef van Dijk <stef@vandijkstef.nl>
2023-04-19 09:57:29 +0200

    Add 'path' input
    In case your package is not in your root directory.

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ try {
   }
 
   const token = core.getInput('token');
+  const path = core.getInput('path');
+  const packagePath = path ? path.join(path, packagePath) : 'package.json';
   const headers = {};
   if (token) {
     core.info('Using specified token');
@@ -20,13 +22,13 @@ try {
   const headSha = github.context.payload.pull_request.head.sha;
 
   core.info(`Comparing ${ headSha } to ${ baseSha }`);
-  const baseUrl = `https://raw.githubusercontent.com/${ github.context.repo.owner }/${ github.context.repo.repo }/${ baseSha }/package.json`
+  const baseUrl = `https://raw.githubusercontent.com/${ github.context.repo.owner }/${ github.context.repo.repo }/${ baseSha }/${packagePath}`
 
   fetch(baseUrl, { headers })
     .then(res => res.json())
     .then(res => res.version)
     .then(version => {
-      const localVersion = require(`${ process.env.GITHUB_WORKSPACE }/package.json`).version;
+      const localVersion = require(`${ process.env.GITHUB_WORKSPACE }/${packagePath}`).version;
 
       if (!semver.valid(localVersion)) core.setFailed(`Current version '${ localVersion }' detected as invalid one`);
       if (!semver.gt(localVersion, version)) core.setFailed(`Version '${ localVersion }' wasn't detected as greater than '${ version }'`);
